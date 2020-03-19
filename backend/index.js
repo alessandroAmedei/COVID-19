5c0977823855a3cb4227090800fdf583e8327716
alessandro <alessandro.amedei@stud.unifi.it>
2020-03-19 17:23:07 +0100

    cors heroku fixing

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,24 @@ app.get('/api',(req,res)=>{
 
 app.use('/api',andamento_nazionale_router);
 
+//IMPORTANTE MIDDLEWARE
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true); questo non ci serve per ora
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port,(err)=>{
---
+++ frontend/covid-19/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<Chart :chartdata="chartdata" :options="options"/>
+</template>
+<script>
+import Chart from './components/Chart.vue'
+export default{
+  components: {
+    Chart
+  },
+    data: () => ({
+    chartdata: {
+      labels: ['January', 'February',4,3,5,7,4,4,7,45,4],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [40, 20,4,3,6,8,5,3,6,8,6]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+  mounted(){
+    this.axios.get('https://covid-19-virus.herokuapp.com/api/andamento_nazionale',headers: {
+          // remove headers
+        })
+    .then(datas =>{
+      datas.forEach(element => {
+        window.console.log(element)
+      });
+    })
+  }
+}
+</script>
