e0d62376dd4643723d71e2a1cf1e0416ab7bc043
alessandro <alessandro.amedei@stud.unifi.it>
2020-03-21 18:24:40 +0100

    added info

---
+++ README.md
@@ -0,0 +1,6 @@
+Made by: Alessandro Amedei
+alessandroamedei18@gmail.com
+
+Frontend is hosted by Firebase: https://covid19-alessandroamedei.firebaseapp.com/
+Backend is hosted by Heroku: https://covid-19-virus.herokuapp.com/api/
+DB is hosted by MongoDB Atlas
--- frontend/covid-19/src/views/Info.vue
+++ frontend/covid-19/src/views/Info.vue
@@ -1,9 +1,20 @@
 <template>
     <v-content>
-        <v-container fluid>
+        <v-container class="center" fluid>
             <h3>Info</h3>
             <br>
-            <h4>In caso di problemi o per informazioni, contattami all'indirizzo email:  alessandroamedei18@gmail.com</h4>
+            <span class="span">In caso di problemi o per informazioni, contattami all'indirizzo email:  alessandroamedei18@gmail.com</span>
+            <br>
+            <br>
+            <h3>Repository dati e codice</h3>
+            <br>
+            <span class="span">Il codice sorgente è pubblico sotto licenza GNU General Public License 3.0 e lo trovi <a href="https://github.com/alessandroAmedei/COVID-19"> qui</a></span>
+
         </v-container>
     </v-content>
 </template>
+<style>
+.center{
+    text-align:center;
+}
+</style>
