9eb10c7748675ba3c995e57c193b5514082b1ecd
alessandro <alessandro.amedei@stud.unifi.it>
2020-03-22 00:05:50 +0100

    added counter at heroku

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const andamento_nazionale_router = require('./api/andamento_nazionale/db');
 const andamento_regionale_router = require('./api/andamento_regionale/db');
+const counter_model = require('./api/counter/counter');
 
 //IMPORTANTE MIDDLEWARE
 app.use(function (req, res, next) {
@@ -23,18 +24,37 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use(function (req, res, next) {
 
-app.get('/api',(req,res)=>{
-    res.send('Benvenuti in questa REST API per la pandemia di COVID-19 in Italia. I dati sono ottenuti ogni giorno verso le 18.30 circa dalla Protezione Civile, FORZA ITALIA!')
+    console.log(req.url);
+    if (req.url === '/api/') {
+        next();
+        return;
+    }
+    counter_model.findById({ _id: 'justone' }, (err, counter) => {
+
+        counter.counter = counter.counter + 1;
+        counter.save(() => {
+            console.log('Request. n (could be duplicate)', counter.counter);
+        });
+    });
+    next();
+});
+
+app.get('/api', (req, res) => {
+    counter_model.findById({ _id: 'justone' }, (err, counter) => {
+        res.send('Benvenuti in questa REST API per la pandemia di COVID-19 in Italia. I dati sono ottenuti ogni giorno verso le 18.30 circa dalla Protezione Civile, FORZA ITALIA! c:' + counter.counter);
+    });
+
 })
 
-app.use('/api',andamento_nazionale_router);
+app.use('/api', andamento_nazionale_router);
 
-app.use('/api',andamento_regionale_router);
+app.use('/api', andamento_regionale_router);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,(err)=>{
-    console.log('app is running on port: ',port);
+app.listen(port, (err) => {
+    console.log('app is running on port: ', port);
 })
 
